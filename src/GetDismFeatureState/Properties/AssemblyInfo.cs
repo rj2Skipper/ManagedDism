c7da9431e609c0411c11e1c57b5c80888d6cb800
Ralf Jänike <jaenike@ferrari-electronic.de>
2021-02-17 13:12:21 +0100

    update project file

---
+++ src/GetDismFeatureState/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GetDismFeatureState")]
+[assembly: AssemblyDescription("Print installed state of of windows features utilizing DISM API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ferrari electronic AG")]
+[assembly: AssemblyProduct("OfficeMaster issuitehelpX")]
+[assembly: AssemblyCopyright("Copyright © Ferrari electronic AG 2019-2020")]
+[assembly: AssemblyTrademark("OfficeMaster")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64db2e42-086b-4194-ba5a-552efa858714")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.31")]
+[assembly: AssemblyFileVersion("1.0.0.31")]
